de7315217397d121117c9357c29876dc0adbef30
Richard Maldonado <rlk538@my.utsa.edu>
2021-10-25 15:01:17 -0500

    Fixed fxml file on main anchorPane

--- SemesterProject/src/application/Main.java
+++ SemesterProject/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("SemesterProject.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("SemesterProject0.fxml"));
 			Scene scene = new Scene(root,650,450);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
