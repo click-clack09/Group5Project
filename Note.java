77e69a6f728424ae3604ce248016b7a87a31ec3a
slimefest <47671940+slimefest@users.noreply.github.com>
2021-11-08 16:27:18 -0600

    Add files via upload
    Reformatted model

--- Note.java
+++ Note.java
@@ -2,4 +2,39 @@ package application.model;
 
 public class Note {
 
+	private String imagePath;
+	private String text;
+	
+	/*
+	 * CONSTRUCTORS
+	 */
+	
+	public Note(String imagePath, String text) {
+		this.imagePath = imagePath;
+		this.text = text;
+	}
+
+	/*
+	 * GETTERS
+	 */
+	
+	public String getImagePath() {
+		return imagePath;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	/*
+	 * SETTERS
+	 */
+	
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
 }
