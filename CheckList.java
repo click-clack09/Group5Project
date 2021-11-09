580f3bcd993bb53f43018cc979cc050f2e7dc2ad
slimefest <47671940+slimefest@users.noreply.github.com>
2021-11-09 14:04:43 -0600

    Added stuff for tables

---
+++ CheckList.java
@@ -0,0 +1,19 @@
+package application.model;
+
+import java.util.ArrayList;
+
+public class CheckList<T> extends ArrayList{
+
+	private ArrayList<T> unchecked; //list of tasks
+	//Boolean checked/unchecked
+	//Just use an ArrayList of objects from the controller?
+	//private ArrayList<T> checked;
+	
+	//public Checklist<T> () {
+	//	unchecked = new ArrayList<T>();
+	//	checked = new ArrayList<T>();
+	//}
+	
+	
+	
+}
