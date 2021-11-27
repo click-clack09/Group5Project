401873a9ba1d32b322a369e6fffaf21da5dc4fd1
DONALD ANDRE MOORE <donmoore26762671@gmail.com>
2021-11-26 18:42:01 -0600

    Donny's comments for DatabaseConnection model

--- SemesterProject/src/application/model/DatabaseConnection.java
+++ SemesterProject/src/application/model/DatabaseConnection.java
@@ -4,15 +4,32 @@ package application.model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/**This Class is used as a generic database connection to the MySQL
+ * Community Server
+ * 
+ * This class contains a URL field, username, password, and database field.
+ * These fields serve as the credentials for setting up the 
+ * application's database connection.
+ */
 public class DatabaseConnection {
 	private static final String URL = "jdbc:mysql://localhost:3306/";
 	private static final String USERNAME = "root";
-	private static final String PASSWORD = "31415!Steel180";
+	private static final String PASSWORD = "yanula20";
 	private static final String DATABASE = "lifehub";
 
+	/**This Class is non-parameterizd constructor used to instantiate the 
+	 * Database Connection class
+	 * 
+	 */
 	public DatabaseConnection() {
 
 	}
+	
+	/**This is a static method is used to create the connection to the application's
+	 * database
+	 * 
+	 * @return a Connection object
+	 */
 	public static Connection getConnection() {
     	Connection connection;
     	try {
