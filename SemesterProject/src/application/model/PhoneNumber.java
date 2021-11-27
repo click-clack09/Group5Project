bcb268adf66c83bf33273cc59fc7946f7b05f812
DONALD ANDRE MOORE <donmoore26762671@gmail.com>
2021-11-26 18:47:39 -0600

    Donny's comments - PhoneNumber model

--- SemesterProject/src/application/model/PhoneNumber.java
+++ SemesterProject/src/application/model/PhoneNumber.java
@@ -1,57 +1,70 @@
 package application.model;
 
+/**The PhoneNumber Class is used to create phone numbers for the user's contacts
+ * 
+ * This class has a number field represented as a String
+ * This class has a type (of phone number) represented as a String
+ */
 public class PhoneNumber
 {
 	private String number;
 	private String type;
+
 	
-	/*
-	 * CONSTRUCTORS
+	/**This a parameterized constructor for the PhoneNumber class
+	 * 
+	 * @param number -String that represents the email adress
+	 * @param type - String that represents the email type is set to default
+	 * @return none
 	 */
-	
 	public PhoneNumber(String number)
 	{
 		this.number = number;
 		type = "default";
 	}
 	
+	/**This a parameterized constructor for the PhoneNumber class
+	 * 
+	 * @param number - String that represents the phone number
+	 * @param type - String to be used as the phone number type
+	 * @return none
+	 */
 	public PhoneNumber(String number, String type)
 	{
 		this.number = number;
 		this.type = type;
 	}
 
-	/*
-	 * GETTERS
-	 */
-	
-	/**
-	 * @return the number
+	/** This is a getter that retreives phone number as a string
+	 * 
+	 * @return number, String
 	 */
 	public String getNumber() {
 		return number;
 	}
 	
-	/**
-	 * @return the type
+	/** This is a getter that retreives the email address type as a string
+	 * 
+	 * @return type, String
 	 */
 	public String getType() {
 		return type;
 	}
 	
-	/*
-	 * SETTERS
-	 */
 
-	/**
-	 * @param number the number to set
+	/** This a setter that sets the phone number
+	 * 
+	 * @param String number
+	 * @return none
 	 */
 	public void setNumber(String number) {
 		this.number = number;
 	}
 
-	/**
-	 * @param type the type to set
+	/** This a setter that sets phone number type
+	 * 
+	 * @param String type
+	 * @return none
 	 */
 	public void setType(String type) {
 		this.type = type;
