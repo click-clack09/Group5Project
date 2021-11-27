e7ae83559c64a76d67069ac3cf62b0315ee03fec
DONALD ANDRE MOORE <donmoore26762671@gmail.com>
2021-11-26 18:45:17 -0600

    Donny's comments - LifeHub model

--- SemesterProject/src/application/model/LifeHub.java
+++ SemesterProject/src/application/model/LifeHub.java
@@ -3,7 +3,13 @@ package application.model;
 import java.net.URL;
 import java.util.ArrayList;
 
-
+/**This Class is used to create LifeHub's for the logged-in user
+ * 
+ * 
+ * LifeHub's have a name, event type, and ArrayLists that hold
+ * LifeHub events, tasks, notes, or classes
+ *
+ */
 public class LifeHub implements Comparable {
 	private String hubName;
 	private int eventType;
@@ -12,23 +18,26 @@ public class LifeHub implements Comparable {
 	private ArrayList<Note> notes;
 	private ArrayList<SchoolClass> classes;
 	
-	
-	//user class has the following:
-	//private static URL last;
-//	private static String username;
-//	private static int userID;
-//	private static String currentHub;
-		
 
-	//this will be a Hub (1 school, 3 personal, or 2 business)
-	//it should a user, category/event Type, a name, an ArrayList of events, tasks, and notes.
-	//for the calendar to show all of the users events, query for all of the users events.
-	//This is independent of the hub currently instantiated.
+	/**This a non-parameterized constructor used for testing the LifeHub class
+	 * 
+	 * @param listName - String to be used as the contact's name
+	 * 
+	 *  This constructor assigns a name to the LifeHub and initializes 3 ArrayLists
+	 *  that will hold tasks, notes, and classes.
+	 */
 	public LifeHub()
 	{
 		hubName ="";
 	}
 	
+	/**This a parameterized constructor for the LifeHub Class
+	 * 
+	 * @param listName - String to be used as the contact's name
+	 * 
+	 *  This constructor assigns a name to the LifeHub and initializes 3 ArrayLists
+	 *  that will hold tasks, notes, and classes.
+	 */
 	public LifeHub(String listName)
 	{
 		this.hubName = listName;
@@ -37,6 +46,16 @@ public class LifeHub implements Comparable {
 		classes= new ArrayList<SchoolClass>();
 	}
 
+	
+	/**This a parameterized constructor for the contact class
+	 * 
+	 * @param hubName - String represents the Hub Name
+	 * @param eventType - int represents a type of event
+	 * @param events - represents an ArrayList of HubEvent objects
+	 * @param tasks - represents an ArrayList of Task objects
+	 * @param notes - represents an ArrayList of Note objects
+	 * @param classes - represents an ArrayList SchoolClass objects
+	 */
 	public LifeHub(String hubName, int eventType, ArrayList<HubEvent> events, ArrayList<Task> tasks,
 			ArrayList<Note> notes) {
 		super();
@@ -49,105 +68,129 @@ public class LifeHub implements Comparable {
 		
 	}
 
-	/**
-	 * @return the hubName
+	/**This a getter that retrieves the LifeHub name
+	 * 
+	 * @return String
 	 */
 	public String getHubName() {
 		return hubName;
 	}
 
-	/**
-	 * @return the eventType
+	/**This a getter that retrieves the LifeHub event type
+	 * 
+	 * @return int
 	 */
 	public int getEventType() {
 		return eventType;
 	}
 
-	/**
-	 * @return the events
+	/**This a getter that retrieves an array list of Hub Events
+	 * 
+	 * @return ArrayList
 	 */
 	public ArrayList<HubEvent> getEvents() {
 		return events;
 	}
 
-	/**
-	 * @return the tasks
+	/**This a getter that retrieves an array list of Task objects
+	 * 
+	 * @return ArrayList
 	 */
 	public ArrayList<Task> getTasks() {
 		return tasks;
 	}
 
-	/**
-	 * @return the notes
+	/**This a getter that retrieves an array list of Note objects
+	 * 
+	 * @return ArrayList
 	 */
 	public ArrayList<Note> getNotes() {
 		return notes;
 	}
 
-	/**
-	 * @param hubName the hubName to set
+	/**This a setter that sets an array Life Hub name
+	 * 
+	 * @return none
 	 */
 	public void setHubName(String hubName) {
 		this.hubName = hubName;
 	}
 
-	/**
-	 * @param eventType the eventType to set
+	/**This a setter that sets an Life Hub event type
+	 * 
+	 * @return none
 	 */
 	public void setEventType(int eventType) {
 		this.eventType = eventType;
 	}
 
-	/**
-	 * @param events the events to set
+	/**This a setter that sets an Life Hub's ArrayList of events
+	 * 
+	 * @return none
 	 */
 	public void setEvents(ArrayList<HubEvent> events) {
 		this.events = events;
 	}
 
-	/**
-	 * @param tasks the tasks to set
+	/**This a setter that sets an Life Hub's ArrayList of tasks
+	 * 
+	 * @return none
 	 */
 	public void setTasks(ArrayList<Task> tasks) {
 		this.tasks = tasks;
 	}
 
-	/**
-	 * @param notes the notes to set
+	/**This a setter that sets an ArrayList of notes
+	 * 
+	 * @return none
 	 */
 	public void setNotes(ArrayList<Note> notes) {
 		this.notes = notes;
 	}
 
+	/**This method is used to compare LifeHub objects
+	 * 
+	 * @return 1 for match, - 1 for non-match
+	 */
 	@Override
 	public int compareTo(Object o) {
 		
 		if (this.equals(o))
-			return 1;// TODO Auto-generated method stub
+			return 1;
 		return -1;
 	}
 	
+	/**This method is used to compare Hub Name strings
+	 * 
+	 * @return the event type for match, - 1 for non-match
+	 */
 	public int compareTo(String str) {
 		
 		if (this.hubName.equals(str))
-			return this.eventType;// TODO Auto-generated method stub
+			return this.eventType;
 		return -1;
 	}
 	
-	/**
-	 * @return the classes
+	/**This a getter that retrieves an array list of SchoolClass objects
+	 * 
+	 * @return ArrayList
 	 */
 	public ArrayList<SchoolClass> getClasses() {
 		return classes;
 	}
 
-	/**
-	 * @param classes the classes to set
+	/**This a getter that retrieves an array list of SchoolClass objects
+	 * 
+	 * @return ArrayList
 	 */
 	public void setClasses(ArrayList<SchoolClass> classes) {
 		this.classes = classes;
 	}
 
+	/**This method is used to compare Life Hub objects
+	 * 
+	 * @return 1 for a match, -1 for a non-match
+	 */
 	public int equals(LifeHub hub)
 	{
 		System.out.println("Equals for lifehub");
@@ -155,7 +198,4 @@ public class LifeHub implements Comparable {
 			return 1;
 		return -1;
 	}
-	
-	
-	
 }
